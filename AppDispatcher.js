550ae8c440d795bcf87b34bdc10b28d5ea19c67b
Jess Kenney <jess@jesskenney.com>
2018-12-18 18:41:57 -0500

    Log action names & exp publish

--- AppDispatcher.js
+++ AppDispatcher.js
@@ -6,16 +6,16 @@ const dispatcher = new Dispatcher();
 
 function logDispatch(action) {
   switch (action.type) {
-  case 'loaded':
-    console.log('Action: loaded (body omitted)');
-    return;
-  case 'migrate-from-images2':
-    console.log('Action: migrate-from-images2 ' + action.images2.length + ' images (body omitted)');
-    return;
-  default: {
-    console.log('Action:', action)
-    return
-  }
+    case 'loaded':
+      console.log('Action: loaded (body omitted)');
+      return;
+    case 'migrate-from-images2':
+      console.log('Action: migrate-from-images2 ' + action.images2.length + ' images (body omitted)');
+      return;
+    default: {
+      console.log('Action:', action.type, action);
+      return;
+    }
   }
 }
 
