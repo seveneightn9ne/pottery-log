71ea8e311191c6d0f89709d63f823f409bc973cf
Jess Kenney <jess@jesskenney.com>
2019-03-31 18:33:32 -0400

    Add version in settings

--- src/views/SettingsPage.tsx
+++ src/views/SettingsPage.tsx
@@ -63,6 +63,7 @@ export default class SettingsPage extends React.Component<SettingsPageProps, Set
           <Button title="Export" onPress={this.props.onStartExport} />
           <View style={{height: 20}} />
           <Button title="Import" onPress={this.importPopup} />
+          <Text style={styles.settingsText}>App version: 2.24.5</Text>
       </View>);
     }
 
