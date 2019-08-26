1e623eb2f1fe25196120417f7d98b97b78797584
Jess Kenney <jess@jesskenney.com>
2019-08-25 23:42:45 -0400

    more export thunk tests

--- src/thunks/__tests__/export.test.js
+++ src/thunks/__tests__/export.test.js
@@ -1,5 +1,6 @@
 import * as uploader from "../../utils/uploader";
 import * as exports from "../export";
+import * as imageutils from "../../utils/imageutils";
 
 jest.mock("../../utils/uploader", () => ({
   startExport: jest.fn().mockResolvedValue(),
@@ -13,6 +14,12 @@ jest.mock("../../utils/exports", () => ({
 }));
 jest.mock("expo-file-system", () => ({
   getInfoAsync: jest.fn().mockReturnValue(Promise.resolve({ exists: true }))
+}));
+jest.mock("../../utils/imageutils", () => ({
+  //saveToFilePure: jest.fn().mockResolvedValue("file-uri.jpg"),
+  saveToFilePure: jest
+    .fn()
+    .mockImplementation(l => Promise.resolve("f/" + l.split("/")[1]))
 }));
 
 describe("exportEverything", () => {
@@ -114,11 +121,181 @@ describe("exportEverything", () => {
     expect(uploader.finishExport).toHaveBeenCalled();
   });
 
-  it("Saves and exports images with no files", () => {});
+  it("Saves and exports images with no files", async () => {
+    const { dispatch, dispatchMock } = makeDispatch(() => {
+      return {
+        exports: { exporting: true },
+        images: {
+          images: {
+            "1.jpg": {
+              name: "1.jpg",
+              localUri: "l/1.jpg"
+            },
+            "2.jpg": {
+              name: "2.jpg",
+              remoteUri: "r/2.jpg"
+            }
+          }
+        }
+      };
+    });
+    await dispatch(exports.exportEverything());
+    const dispatchCalls = [
+      {
+        type: "export-status",
+        exportId: expect.any(Number),
+        exporting: true,
+        status: "Starting export..."
+      },
+      {
+        type: "export-status",
+        exportId: expect.any(Number),
+        exporting: true,
+        status: "Saving 2 images..."
+      },
+      {
+        type: "export-status",
+        exportId: expect.any(Number),
+        exporting: true,
+        status: "Exporting images (0/2)..."
+      },
+      {
+        type: "export-status",
+        exportId: expect.any(Number),
+        exporting: true,
+        status: "Exporting images (1/2)..."
+      },
+      {
+        type: "export-status",
+        exportId: expect.any(Number),
+        exporting: true,
+        status: "Exporting images (2/2)..."
+      },
+      {
+        type: "export-status",
+        exportId: expect.any(Number),
+        exporting: true,
+        status: "Finishing export..."
+      },
+      {
+        type: "export-finished",
+        exportId: expect.any(Number),
+        uri: "https://export-file-uri"
+      }
+    ];
+    expect(dispatchMock.mock.calls).toEqual(dispatchCalls.map(a => [a]));
+    expect(uploader.startExport).toHaveBeenCalled();
+    expect(imageutils.saveToFilePure).toHaveBeenCalledWith("l/1.jpg", false);
+    expect(imageutils.saveToFilePure).toHaveBeenCalledWith("r/2.jpg", true);
+    expect(uploader.exportImage).toHaveBeenCalledWith("f/1.jpg");
+    expect(uploader.exportImage).toHaveBeenCalledWith("f/2.jpg");
+    expect(uploader.finishExport).toHaveBeenCalled();
+  });
 
-  it("discards images with no image", () => {});
+  it("discards images with no image", async () => {
+    const { dispatch, dispatchMock } = makeDispatch(() => {
+      return {
+        exports: { exporting: true },
+        images: {
+          images: {
+            "1.jpg": {
+              name: "1.jpg"
+            },
+            "2.jpg": {
+              name: "2.jpg",
+              localUri: "l/2.jpg"
+            }
+          }
+        }
+      };
+    });
+    await dispatch(exports.exportEverything());
+    const dispatchCalls = [
+      {
+        type: "export-status",
+        exportId: expect.any(Number),
+        exporting: true,
+        status: "Starting export..."
+      },
+      {
+        type: "export-status",
+        exportId: expect.any(Number),
+        exporting: true,
+        status: "Saving 1 images..."
+      },
+      {
+        type: "export-status",
+        exportId: expect.any(Number),
+        exporting: true,
+        status: "Exporting images (0/1)..."
+      },
+      {
+        type: "export-status",
+        exportId: expect.any(Number),
+        exporting: true,
+        status: "Exporting images (1/1)..."
+      },
+      {
+        type: "export-status",
+        exportId: expect.any(Number),
+        exporting: true,
+        status: "Finishing export..."
+      },
+      {
+        type: "export-finished",
+        exportId: expect.any(Number),
+        uri: "https://export-file-uri"
+      }
+    ];
+    expect(dispatchMock.mock.calls).toEqual(dispatchCalls.map(a => [a]));
+    expect(uploader.startExport).toHaveBeenCalled();
+    expect(imageutils.saveToFilePure).toHaveBeenCalledWith("l/2.jpg", false);
+    expect(uploader.exportImage).toHaveBeenCalledWith("f/2.jpg");
+    expect(uploader.finishExport).toHaveBeenCalled();
+  });
 
-  it("cancels on start-export error", () => {});
+  it("cancels on start-export error", async () => {
+    uploader.startExport.mockReturnValue(
+      Promise.reject("start-export HTTP error")
+    );
+
+    const { dispatch, dispatchMock } = makeDispatch(() => {
+      return {
+        exports: { exporting: true },
+        images: {
+          images: {
+            "1.jpg": {
+              name: "1.jpg",
+              fileUri: "f/1.jpg"
+            },
+            "2.jpg": {
+              name: "2.jpg",
+              fileUri: "f/2.jpg"
+            }
+          }
+        }
+      };
+    });
+    await dispatch(exports.exportEverything());
+    const dispatchCalls = [
+      {
+        type: "export-status",
+        exportId: expect.any(Number),
+        exporting: true,
+        status: "Starting export..."
+      },
+      {
+        type: "export-status",
+        exportId: expect.any(Number),
+        exporting: false,
+        status: "Export failed.\nstart-export HTTP error"
+      }
+    ];
+    expect(dispatchMock.mock.calls).toEqual(dispatchCalls.map(a => [a]));
+    expect(uploader.startExport).toHaveBeenCalled();
+    expect(uploader.exportImage).not.toHaveBeenCalled();
+    expect(uploader.finishExport).not.toHaveBeenCalled();
+  });
 
   it("cancels on export-image error", () => {});
 
